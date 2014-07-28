899cbd6da5bd6d44574e6240afe6ad7f0f16d73e
Gerit Wagner <gerit1989@googlemail.com>
2014-07-28 11:47:45 +0200

    Zeitplan

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
 <!-- <h2>Überschrift</h2>
 <br /> -->
 
+
+<p><h2>Hinweis zu den süddeutschen Meisterschaften (U23/U16)</h2></p>
+
+<p>Aus organisatorischen Gründen wurden vom lokalen Ausrichter Zeitplanänderungen vorgenommen. <a href="/download/Zeitplan_SDM2014_U23U16.pdf">Hier</a> geht's zum vorläufigen Zeitplan</p>
+
+<!--
 <p>Liebe Sportfreunde,</p>
 
 <p>herzlich willkommen auf der Internetpräsenz des Süddeutschen 
@@ -18,6 +24,7 @@ Wichtigkeit sind.</p>
 <p>Mit sportlichen Grüßen</p>
 
 <p>Das Präsidium des SLV
+-->
 </p>
 <p></p>
 <p></p>
