04318488b2966cc2e98d1ddd62fdef503400c53c
Gerit Wagner <gerit1989@googlemail.com>
2014-11-08 23:57:10 +0100

    FTP-Version, 08.11.2014

--- index.php
+++ index.php
@@ -7,30 +7,7 @@
 <br /> -->
 
 
-<p><h2>Hinweis zu den süddeutschen Meisterschaften (U23/U16)</h2></p>
-
-<p>Aus organisatorischen Gründen wurden vom lokalen Ausrichter Zeitplanänderungen vorgenommen. Hier geht's zum vorläufigen <a href="/download/Zeitplan_SDM2014_U23U16.pdf">Zeitplan</a> und zur <a target="_blank" href="http://www.djk-friedberg.de/sueddeutsche_2014/index.php?page=15 ">Veranstaltungswebseite</a></p>
-
-
-<h2>Leichtathletik-EM in Zürich</h2>
-
-<p><strong>Täglich Highlights aus Sport und Show – Reisekosten inbegriffen</strong></p>
-
-<p><img class="float_left" title="Stadion" src="/images/inhalt/stadion_stimmungsbild.jpg" height="180px" >Unterstützte die deutschen Athleten, wenn an den Europameisterschaften (12. bis 17. August) im legendären Letzigrund-Stadion die Leichtathletik auf einzigartige Weise zelebriert wird. Für alle Tage sind noch Eintrittskarten erhältlich. Die in den Tickets inbegriffene Anreise mit dem öffentlichen Verkehr ab Schweizer Grenze macht den Besuch für deutsche Fans besonders attraktiv. Preiswerte Unterkünfte können so auch außerhalb Zürichs gebucht werden, ohne das zusätzliche Reisekosten entstehen.<p>
-
-<p>Mit 1.400 Athletinnen und Athleten aus 50 Nationen ist Zürich 2014 das grösste Sportereignis aller Zeiten für die Gastgeber-Nation. Das legendäre Letzigrund Stadion ist der Hauptschauplatz, unterhaltsame Shows beschliessen täglich das Wettkampfprogramm der besten Athleten Europas. Die Strassenrennen im Gehen und Marathon werden auf attraktiven Strecken im Zentrum der Metropole am Zürisee ausgetragen.</p>
-
-<p>Und das City Festival zur EM mit vielen Attraktionen, Aktivitäten und Gewinnspielen findet ebenfalls in der Stadtmitte auf dem Sechseläutenplatz vor der imposanten Kulisse des Opernhauses statt. Im Mittelpunkt steht das eigens aufgebaute House of Switzerland, das zuletzt als Repräsentanz der Schweiz bei den Olympischen Winterspielen in Sotschi für Furore sorgte. Hier und im Stadion sind an allen EM-Tagen Höhepunkte aus Sport und Unterhaltung zu erleben.</p>
-
-<p>Zuschauern aus Deutschland wird die Anreise leicht gemacht: Die Nutzung aller öffentlichen Verkehrsmittel ab Grenzübergang in die Schweiz (z. B. Schaffhausen, Basel, Buchs/St. Gallen) – egal ob per Bahn, Bus, Schiff oder Strassenbahn – ist an den Geltungstagen in der Eintrittskarte bereits enthalten. So sind für echte Leichtathletik-Fans auch preiswerte Unterkünfte jenseits der Grenze noch am Abend praktisch ohne weitere Kosten erreichbar.</p>
-
-<p><a href="www.zuerich2014.ch">www.zuerich2014.ch</a></p>
-
-<p style="text-align: center"><img title="Stadion" src="/images/inhalt/LEM2014_ml_pt_rgb_l.jpg" height="250px" ></p>
-
-
-<!--
-<p>Liebe Sportfreunde,</p>
+<p><strong>Liebe Sportfreunde,</strong></p>
 
 <p>herzlich willkommen auf der Internetpräsenz des Süddeutschen 
 Leichtathletik - Verbandes.</p>
@@ -42,10 +19,10 @@ Wichtigkeit sind.</p>
 <p>Mit sportlichen Grüßen</p>
 
 <p>Das Präsidium des SLV
--->
+
+
 </p>
 <p></p>
 <p></p>
-<p style="font-size:0.8em">Foto: Orga-Komittee, Zürich</p>
 <br />
 <?php require("_footer.inc.php"); ?>
