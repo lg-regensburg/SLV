dabaed45eedf0ea6adbe07ab5a19f3ede709d4ee
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2015-03-01 21:50:35 +0100

    Ticketaktion gelöscht

--- index.php
+++ index.php
@@ -15,19 +15,6 @@
 
 <p><a href="http://www.leichtathletik.de/news/news/detail/yasmin-kwadwo-knackt-die-hallen-em-norm-in-karlsruhe/">Tag 1</a>, <a href="http://www.leichtathletik.de/news/news/detail/tamara-schassberger-kratzt-an-u18-bestleistung/">Tag 2</a></p>
 
-
-<p><strong>Besonderes Angebot für Titelkämpfe in Nürnberg: Familien-Special für DM im Grundig-Stadion</strong></p>
-
-<p>Für die Deutschen Leichtathletik-Meisterschaften in Nürnberg (24.–26. Juli) hat der Deutsche Leichtathletik-Verband (DLV) im Februar ein ganz besonderes Angebot für Familien vorbereitet: Ausschließlich im Zeitraum 1. bis einschließlich 28. Februar zahlen zwei Erwachsene und ein Kind in der Kategorie I (Gegentribüne) statt 64,50 Euro nur 39 Euro für einen Veranstaltungstag. Für jedes weitere Kind werden 5 Euro berechnet.</p>
-
-<p align="center"><img width="240px" src="http://blv-sport.de/uploads/pics/DLV_Logo_rot_ohne_02.jpg"></p>
-
-<p>Zu buchen ist das so genannte Familien-Special für die DM im Grundig-Stadion nur unter 01806-999 0000* sowie bei den Vorverkaufsstellen der "Nürnberger Nachrichten". Zu den Deutschen Meisterschaften in Nürnberg werden über 1000 Athletinnen und Athleten erwartet, die um die Titel und die Qualifikation für die Weltmeisterschaften in Peking kämpfen. Der Weitsprung findet bereits am Freitag (24. Juli) auf dem Nürnberger Hauptmarkt statt. Im Vorverkauf wurden für die Deutschen Meisterschaften in Nürnberg bisher bereits 14 000 Tickets abgesetzt.</p>
-
-
-<p>*(€ 0,20/Verbindung aus dem dt. Festnetz / max. € 0,60/Verbindung aus dem dt. Mobilfunknetz)</p>
-
-<!-- 
 <p><strong>Liebe Sportfreunde,</strong></p>
 
 <p>herzlich willkommen auf der Internetpräsenz des Süddeutschen 
@@ -39,11 +26,8 @@ Wichtigkeit sind.</p>
 
 <p>Mit sportlichen Grüßen</p>
 
-<p>Das Präsidium des SLV
--->
+<p>Das Präsidium des SLV</p>
 
-
-</p>
 <p></p>
 <p></p>
 <br />
