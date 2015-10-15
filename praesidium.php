3ee64a243a909a508568812ff3838b6199134e94
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2015-10-15 07:58:27 +0200

    update praesidium

--- praesidium.php
+++ praesidium.php
@@ -8,32 +8,44 @@
 
     <table class="MsoNormalTable" border="1" cellspacing="0" cellpadding="0" style="border-collapse: collapse; border: medium none" id="table1" width="656">
       <tr>
-        <td valign="top" style="width: 318px; border: 1.0pt solid windowtext; padding-left: 3.5pt; padding-right: 3.5pt; padding-top: 0cm; padding-bottom: 0cm">
+        <td valign="top" style="width: 315px; border-left: 1.0pt solid windowtext; border-right: 1.0pt solid windowtext; border-top: 1.0pt solid windowtext; border-bottom: 1.0pt solid windowtext; padding-left: 3.5pt; padding-right: 3.5pt; padding-top: 0cm; padding-bottom: 0cm">
           <p>
             <strong>Präsident</strong>
           </p>
-          <p>Dr. Ulrich Becker</p>
-          <p>Nelkenstr. 25</p>
-          <p>67691 Hochspeyer</p>
-          <p>Fon: 06305 / 5320 (p), 0631 / 201040 (d)</p>
-          <p>Fax: 0631 / 20104-23 (d)</p>
-          <p>Mail: Dr.U.Becker(at)gmx.de</p>
-          <td valign="top" style="width: 315px; border-left: medium none; border-right: 1.0pt solid windowtext; border-top: 1.0pt solid windowtext; border-bottom: 1.0pt solid windowtext; padding-left: 3.5pt; padding-right: 3.5pt; padding-top: 0cm; padding-bottom: 0cm">
-            <p>
-              <strong>Vizepräsident</strong>
-            </p>
-            <p>
-              Jochen Schweitzer (BLV) </p>
-            <p>
-              Geislinger Str. 12A</p>
-            <p>
-              85435 Erding</p>
-            <p>
-              Fon: 08122-8683790</p>
-            <p>
-              Mobil: 0175-4122728</p>
-            <p>
-              Mail: schweitzer (at) blv-sport.de </td>
+          <p>
+            Jochen Schweitzer (BLV) </p>
+          <p>
+            Geislinger Str. 12A</p>
+          <p>
+            85435 Erding</p>
+          <p>
+            Fon: 08122-8683790</p>
+          <p>
+            Mobil: 0175-4122728</p>
+          <p>
+            Mail: schweitzer (at) blv-sport.de </p>
+        </td>
+        <td valign="top" style="width: 318px; border: 1.0pt solid windowtext; padding-left: 3.5pt; padding-right: 3.5pt; padding-top: 0cm; padding-bottom: 0cm">
+          <p>
+            <strong>Vizepräsident</strong>
+          </p>
+          <p>Paul Blaschke</p>
+          <p>Dammstr. 20</p>
+          <p>55459 Grolsheim</p>
+          <p>Telefon:: 06727 / 5855</p>
+          <p>Fax: 06727 / 952988</p>
+          <p>Mail: paul.blaschke(at)t-online.de</p>
+        </td>
+        <td valign="top" style="width: 318px; border: 1.0pt solid windowtext; padding-left: 3.5pt; padding-right: 3.5pt; padding-top: 0cm; padding-bottom: 0cm">
+          <p>
+            <strong>Vizepräsident Sport</strong>
+          </p>
+          <p>Hans Krieg</p>
+          <p>Schillerstr. 4</p>
+          <p>72663 Grossbettlingen</p>
+          <p>Tel. 07022 / 90 454 95</p>
+          <p>Mail: krieg.hans (at) t-online.de</p>
+        </td>
       </tr>
       <tr>
         <td valign="top" style="width: 318px; border-left: 1.0pt solid windowtext; border-right: 1.0pt solid windowtext; border-top: medium none; border-bottom: 1.0pt solid windowtext; padding-left: 3.5pt; padding-right: 3.5pt; padding-top: 0cm; padding-bottom: 0cm">
@@ -45,7 +57,7 @@
           <p>Tel.: 069/6789211 bis 213</p>
           <p>Fax: 069/679708</p>
           <p>Internet: <a position="37" style="text-decoration: underline; text-underline: single" href="http://www.hlv.de/">www.hlv.de</a></p>
-          <p>E-Mail:Info@HLV.de</p>
+          <p>Mail:Info (at) HLV.de</p>
           <td valign="top" style="width: 315px; border-left: medium none; border-right: 1.0pt solid windowtext; border-top: medium none; border-bottom: 1.0pt solid windowtext; padding-left: 3.5pt; padding-right: 3.5pt; padding-top: 0cm; padding-bottom: 0cm">
             <p>
               <strong>Schatzmeister</strong>
@@ -56,6 +68,18 @@
             <p>Fon: 06359/872763 </p>
             <p>Mail: paulpaszyna(at)aol.com
               <p>
+          </td>
+          <td valign="top" style="width: 315px; border-left: medium none; border-right: 1.0pt solid windowtext; border-top: medium none; border-bottom: 1.0pt solid windowtext; padding-left: 3.5pt; padding-right: 3.5pt; padding-top: 0cm; padding-bottom: 0cm">
+            <p>
+              <strong>Kassenprüferin</strong>
+            </p>
+            <p>Anja Wolf-Blanke</p>
+            </p>
+            <p>Goethestraße 1</p>
+            <p>64560 Riedstadt</p>
+            <p>Tel. 06158 6750</p>
+            <p>Fax 06158 5786</p>
+            <p>Mail: Anja.Wolf-Blanke (at) hlv.de</p>
           </td>
       </tr>
       <tr>
@@ -74,51 +98,7 @@
           <p>
             Fax: 06202 / 780 111</p>
           <p> </td>
-        <td valign="top" style="width: 315px; border-left: medium none; border-right: 1.0pt solid windowtext; border-top: medium none; border-bottom: 1.0pt solid windowtext; padding-left: 3.5pt; padding-right: 3.5pt; padding-top: 0cm; padding-bottom: 0cm">
-          <p>
-            <strong>Sprecher der Kampfrichterwarte</strong>
-          </p>
-          <p>
-            Rolf-Achim Dörr</p>
-          <p>
-            Wehrdaer Weg 4</p>
-          <p>
-            35037 Marburg</p>
-          <p>
-            Fon: 06421 / 657 44</p>
-          <p>
-            Fax: 06421 / 62 02 04</p>
-          <p>
-            Mail: rolf-achim.doerr(at)web.de
-            <p>
-              </td>
-      </tr>
-      <tr>
-        <td valign="top" style="width: 318px; border-left: 1.0pt solid windowtext; border-right: 1.0pt solid windowtext; border-top: medium none; border-bottom: 1.0pt solid windowtext; padding-left: 3.5pt; padding-right: 3.5pt; padding-top: 0cm; padding-bottom: 0cm">
-          <p>
-            <strong>Koordination der Ausschreibungen</strong>
-          </p>
-          <p>
-            Dieter Locher</p>
-          <p>
-            Renninger Straße 12</p>
-          <p>
-            71063 Sindelfingen</p>
-          <p>
-            Fon: 07031 / 800 487</p>
-          <p>
-            Fax: 07031 / 789069
-          </p>
-          <p>
-            Mail: locher(at)wlv-sport.de
-            <p> </td>
-        <td valign="top" style="width: 315px; border-left: medium none; border-right: 1.0pt solid windowtext; border-top: medium none; border-bottom: 1.0pt solid windowtext; padding-left: 3.5pt; padding-right: 3.5pt; padding-top: 0cm; padding-bottom: 0cm">
-          <p>
-            <strong>Kassenprüfer</strong>
-          </p>
-          <p> </p>
-          <p>
-            N.N.</td>
+
       </tr>
       <tr>
         <td valign="top" style="border-left: 1.0pt solid windowtext; border-right: 1.0pt solid windowtext; border-top: medium none; border-bottom: 1.0pt solid windowtext; padding-left: 3.5pt; padding-right: 3.5pt; padding-top: 0cm; padding-bottom: 0cm" colspan="2">
