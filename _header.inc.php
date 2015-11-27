eaa0344365ee8179da0af33a0205fc7126ee6502
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2015-11-27 15:38:18 +0100

    change header language to german

--- _header.inc.php
+++ _header.inc.php
@@ -7,7 +7,7 @@ $mA = $menu['active'];
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
 <head>
 <title><?php if (!empty($title)) echo $title." - "; ?>Süddeutscher Leichtathletik Verband</title>
 <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
