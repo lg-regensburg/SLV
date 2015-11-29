e538004537b15b7bac0a5aa8c8b0f744d70b2189
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2015-11-29 12:27:03 +0100

    update ausschreibungen

--- ausschreibungen.php
+++ ausschreibungen.php
@@ -5,6 +5,16 @@
 
     <h2>Ausschreibungen</h2>
     <br />
+    <p><a target="_blank" href="http://www.sdm2015.de/ ">SDM Aktive / U18 </a></p>
+    <p><a target="_blank" href="/download/Ausschreibung_SDM_2015.pdf">SDM U23 / U16</a></p>
+    <p><a target="_blank" href="/download/Ausschreibung_SDHM_U18.pdf">SDHM Aktive / U 18</a></p>
+    <p><a target="_blank" href="/download/Meldeliste-Süddt.Halle-Akt-U18+Rahmenwettb.pdf">SDHM Aktive / U 18 Teilnehmer</a></p>
+    <p><a target="_blank" href="/download/Ausschreibung%20Bayer%20und%20S%C3%BCddeutsche%2020140521%2013%20Uhr.pdf">SDM Gehen</a></p>
+    <p><a target="_blank" href="http://www.hlv.de/WETTKAMPFSPORT/view_termin.asp?id=975">SDM Bahngehen</a></p>
+    <p><a target="_blank" href="http://www.lvrheinhessen.de/html/Termine-2015/Ausschreibungen-2015/150919-55%20SLV%20Senioren-MK%202015.pdf">SDM Senioren MK</a></p>
+    <p><a target="_blank" href="/download/Allgemeine%20Ausschreibungsbestimmungen%20Aenderung.pdf">Ausschreibungsbestimmungen</a></p>
+    <p><a target="_blank" href="/download/Normen_SDM.pdf">Normen</a></p>
+<!---
     <p><a target="_blank" href="http://www.sdm2015.de/">SDM Aktive / U18 </a></p>
     <p><a target="_blank" href="/download/Ausschreibung_SDM_2015.pdf">SDM U23 / U16</a></p>
     <p><a target="_blank" href="/download/Ausschreibung_SDHM_U18.pdf">SDHM Aktive / U 18</a></p>
@@ -14,5 +24,6 @@
 <p><a target="_blank" href="/download/Allgemeine%20Ausschreibungsbestimmungen%20%C3%84nderung.pdf">Ausschreibungsbestimmungen</a></p>
 <p><a target="_blank" href="/download/Normen_SDM.pdf">Normen</a></p>
 
+-->
 
-<?php require("_footer.inc.php "); ?>
+<?php require("_footer.inc.php"); ?>
