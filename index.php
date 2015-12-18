41be73ad27393fc3279d28eab525876980a652ff
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2015-12-18 20:44:09 +0100

    update artikel

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
 
     <h2>Jochen Schweitzer zum neuen Präsidenten des Süddeutschen Leichtathletik-Verbandes gewählt</h2>
 
-    <p>
-      Der Süddeutsche Leichtathletik-Verband (SLV) wird ab sofort von einem Präsidenten aus Bayern "regiert". Jochen Schweitzer (33) wurde am Samstag beim Verbandsrat in Bietigheim-Bissingen (Baden-Württemberg) zum Nachfolger des nicht mehr kandidierenden Dr. Ulrich Becker (Pfalz) gewählt. Schweitzer amtiert seit 2011 als SLV-Vizepräsident und ist bei der LG Telis Finanz Regensburg für Marketing und Organisation, unter anderem bei der Sparkassen Gala, zuständig. <a href="http://www.blv-sport.de/index.php?id=377&tx_ttnews[tt_news]=2217&cHash=25487bf282057df87d1e5105de0f4657">Hier</a> geht's weiter auf blv-sport.de
+    <p><img src="http://www.blv-sport.de/typo3temp/pics/ec21e4a5b2.jpg" alt="Das neue Präsidium des Süddeutschen Leichtathletik-Verbandes (von links): Paul Blaschke, Hans Krieg, Jochen Schweitzer und Paul Paszyna. (Foto: blv-sport.de)" height="150px"/>
+      Der Süddeutsche Leichtathletik-Verband (SLV) wird ab sofort von einem Präsidenten aus Bayern "regiert". Jochen Schweitzer (33) wurde am Samstag beim Verbandsrat in Bietigheim-Bissingen (Baden-Württemberg) zum Nachfolger des nicht mehr kandidierenden Dr. Ulrich Becker (Pfalz) gewählt. Schweitzer amtiert seit 2011 als SLV-Vizepräsident und ist bei der LG Telis Finanz Regensburg für Marketing und Organisation, unter anderem bei der Sparkassen Gala, zuständig. <a href="http://www.blv-sport.de/index.php?id=377&tx_ttnews[tt_news]=2217&cHash=25487bf282057df87d1e5105de0f4657" target="_blank">Hier</a> geht's weiter auf blv-sport.de
     </p>
 
     <h2>Termine</h2>
