c4072169d6f6a1ac21b0291c2b464336d97f362b
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2015-12-18 20:48:27 +0100

    update termine

--- termine.php
+++ termine.php
@@ -5,7 +5,9 @@
 
     <h2>Termine</h2>
     <br />
+    <p>16.01.2016 Süddeutsche Hallenmeisterschaften im Bahngehen in Karlsruhe</p>
     <p>30.-31.01.2016 SLV-Hallenmeistersch. Aktive/U18 in Sindelfingen</p>
+    <p>12.03.2016 Süddeutsche Meisterschaften Aktive 10.000m (Bahn) in Regensburg</p>
     <p>25.-26.06.2016 SLV-Meisterschaften Aktive/ U18 in Heilbronn</p>
     <p>16.-17.07.2016 SLV-Meisterschaften U23/U16 in St. Wendel</p>
     <p>24.09.2016 SLV-Meisterschaften Seniorenmehrkampf in Nieder-Olm</p>
