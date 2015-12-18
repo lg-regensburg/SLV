7de6ea564dd8e596223bc3eac848de025ae2fb68
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2015-12-18 20:41:56 +0100

    update termine

--- index.php
+++ index.php
@@ -12,6 +12,15 @@
       Der Süddeutsche Leichtathletik-Verband (SLV) wird ab sofort von einem Präsidenten aus Bayern "regiert". Jochen Schweitzer (33) wurde am Samstag beim Verbandsrat in Bietigheim-Bissingen (Baden-Württemberg) zum Nachfolger des nicht mehr kandidierenden Dr. Ulrich Becker (Pfalz) gewählt. Schweitzer amtiert seit 2011 als SLV-Vizepräsident und ist bei der LG Telis Finanz Regensburg für Marketing und Organisation, unter anderem bei der Sparkassen Gala, zuständig. <a href="http://www.blv-sport.de/index.php?id=377&tx_ttnews[tt_news]=2217&cHash=25487bf282057df87d1e5105de0f4657">Hier</a> geht's weiter auf blv-sport.de
     </p>
 
+    <h2>Termine</h2>
+    <br />
+    <p>30.-31.01.2016 SLV-Hallenmeistersch. Aktive/U18 in Sindelfingen</p>
+    <p>25.-26.06.2016 SLV-Meisterschaften Aktive/ U18 in Heilbronn</p>
+    <p>16.-17.07.2016 SLV-Meisterschaften U23/U16 in St. Wendel</p>
+    <p>24.09.2016 SLV-Meisterschaften Seniorenmehrkampf in Nieder-Olm</p>
+    <p>03.09.2016 SLV-Meisterschaften im Bahngehenin Groß Gerau</p>
+    <p>12.07.2016 SLV-Meisterschaften im Straßengehen in Niederaichbach </p>
+
     <p>
       <strong>Liebe Sportfreunde,</strong>
     </p>
--- termine.php
+++ termine.php
@@ -5,10 +5,11 @@
 
     <h2>Termine</h2>
     <br />
-    <p>07.-08.02.2015 SLV-Hallenmeisterschaften Aktive/U18 in Karlsruhe</p>
-    <p>04.-05.07.2015 SLV-Meisterschaften Aktive/ U18 in Kaiserslautern</p>
-    <p>11.-12.07.2015 SLV-Meisterschaften U23/U16 in Kassel</p>
-    <p>20.06.2015 SLV-Meisterschaften Gehen</p>
-    <p>19.09.2015 SLV-Meisterschaften Seniorenmehrkampf Nieder-Olm</p>
+    <p>30.-31.01.2016 SLV-Hallenmeistersch. Aktive/U18 in Sindelfingen</p>
+    <p>25.-26.06.2016 SLV-Meisterschaften Aktive/ U18 in Heilbronn</p>
+    <p>16.-17.07.2016 SLV-Meisterschaften U23/U16 in St. Wendel</p>
+    <p>24.09.2016 SLV-Meisterschaften Seniorenmehrkampf in Nieder-Olm</p>
+    <p>03.09.2016 SLV-Meisterschaften im Bahngehenin Groß Gerau</p>
+    <p>12.07.2016 SLV-Meisterschaften im Straßengehen in Niederaichbach </p>
 
     <?php require("_footer.inc.php"); ?>
