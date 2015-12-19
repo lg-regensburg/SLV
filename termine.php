f7132f48a884054cfec58553ad6c7da490fee13c
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2015-12-19 15:23:16 +0100

    update termine und ausschreibungen

--- termine.php
+++ termine.php
@@ -11,7 +11,7 @@
     <p>25.-26.06.2016 SLV-Meisterschaften Aktive/ U18 in Heilbronn</p>
     <p>16.-17.07.2016 SLV-Meisterschaften U23/U16 in St. Wendel</p>
     <p>24.09.2016 SLV-Meisterschaften Seniorenmehrkampf in Nieder-Olm</p>
-    <p>03.09.2016 SLV-Meisterschaften im Bahngehenin Groß Gerau</p>
+    <p>03.09.2016 SLV-Meisterschaften im Bahngehen in Groß Gerau</p>
     <p>12.07.2016 SLV-Meisterschaften im Straßengehen in Niederaichbach </p>
 
     <?php require("_footer.inc.php"); ?>
