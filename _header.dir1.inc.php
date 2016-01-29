95c1078edb22f2c270ec064abea8ea08cad1b36f
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2016-01-29 21:30:09 +0100

    update header

--- _header.dir1.inc.php
+++ _header.dir1.inc.php
@@ -11,7 +11,7 @@ $mA = $menu['active'];
 <head>
 <title><?php if (!empty($title)) echo $title." - "; ?>Süddeutscher Leichtathletik-Verband</title>
 <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Süddeutscher Leichtathletik-Verband" />
+<meta name="keywords" content="Süddeutscher Leichtathletik Verband, SLV, Leichtathletik, Top-Meeting, Leichtathletik-Elite, Sprint, Hindernis, Hürden" />
 <?php if (!empty($description)) echo "<meta name=\"description\" content=\"$description\" />"; ?>
 <?php /*if ($menu['current']=="live-ticker" && !empty($refresh)) echo $refresh."\n"; */?>
 <meta name="language" content="de" />
@@ -38,7 +38,7 @@ $mA = $menu['active'];
 				<!-- end: skip link navigation -->
 			</div>
 			<div id="flashHeader">
-			  <img src="../images/design/header.jpg" width="900" height="245" alt="Sparkassen Gala 2013" />
+			  <img src="../images/design/header.jpg" width="900" height="245" alt="Süddeutscher Leichtathletik Verband" />
 			</div>
 		</div>
 		<!-- begin: main navigation #nav -->
@@ -46,42 +46,48 @@ $mA = $menu['active'];
 			<!-- skip anchor: navigation -->
 			<a id="navigation" name="navigation"></a>
 			<div id="nav_main">
-				<ul class="adxm menu">
-				  <li<? if($m == 'aktuelles') echo $mA; ?>><a href="../index.php">Aktuelles</a></li>
-				  <!--<li<? if($m == 'news') echo $mA; ?>><a href="../news/">Presse</a>
-				   	<ul>
-				      <li><a href="../news/vorschauen.php">Vorschauen</a></li>
-				      <li><a href="../news/berichte.php">Berichte</a></li>
-				      <li><a href="">Impressionen   »</a>
-						<ul id="navImpressionen">
-						  <li><a href="../news/impressionen-kiefner.php">von Theo Kiefner</a></li>
-						  <!--<li><a href="../news/impressionen-lutz.php">von Ludwig Lutz</a></li>
-						  <li><a href="../news/impressionen-bruessel.php">von Christian Brüssel</a></li>
-						</ul>
-					  </li>
-				    </ul>
-				  </li>-->
-				  <li<? if($m == 'infos') echo $mA; ?>><a href="../infos/">Infos</a>
+        <ul class="adxm menu">
+				  <li<? if($m == 'home') echo $mA; ?>><a href="index.php">Home</a></li>
+				  <li<? if($m == 'veranstaltungen') echo $mA; ?>><a href="ausschreibungen.php">Veranstaltungen</a>
 				    <ul>
-				      <!--<li><a href="../infos/wissenswertes.php">Wissenswertes / <br />Important notes</a></li>-->
-				      <li><a href="../infos/stadt-regensburg.php">Stadt Regensburg</a></li>
-				      <li><a href="../infos/anreise.php">Anreise / Anfahrt</a></li>
-				      <!--<li><a href="../infos/eintrittspreise.php">Eintrittspreise</a></li>-->
-				      <li><a href="../infos/unterkuenfte.php">Unterkünfte</a></li>
-				      <li><a href="../infos/kontakt.php">Kontakt</a></li>
-				      <li><a href="../infos/akkreditierung.php">Presse</a></li>
-				      <li><a href="http://tinyurl.com/6zgldm" target="_blank">Wetter</a></li>
+				      <li><a href="termine.php">Termine</a></li>
+				      <li><a target="_blank" href="http://www.sdm2015.de/ ">SDM Aktive / U18 </a></li>
+				      <li><a target="_blank" href="../download/Ausschreibung_SDM_2016.pdf">SDM U23 / U16</a></li>
+				      <li><a target="_blank" href="../download/Ausschreibung_SDHM_2016.pdf">SDHM Aktive / U 18</a></li>
+				      <li><a target="_blank" href="http://www.blv-sport.de/index.php?id=242&vid=20166557">SDM Gehen</a></li>
+              <li><a target="_blank" href="http://www.hlv.de/WETTKAMPFSPORT/view_termin.asp?id=975">SDM Bahngehen</a></li>
+					  <li><a target="_blank" href="http://www.lvrheinhessen.de/html/Termine-2015/Ausschreibungen-2015/150919-55%20SLV%20Senioren-MK%202015.pdf">SDM Senioren MK</a></li>
+				      <li><a target="_blank" href="../download/Allgemeine_Ausschreibungsbestimmungen_Aenderung.pdf">Ausschreibungs-<br/>bestimmungen</a></li>
+				      <li><a target="_blank" href="../download/Normen_SDM.pdf">Normen</a></li>
+					</ul>
+				  </li>
+				  <li<? if($m == 'ergebnisse') echo $mA; ?>><a href="ergebnisse.php">Ergebnisse</a>
+				  <ul>
+				      <li><a target="_blank" href="http://ladv.de/ergebnis/datei/24012">SDM Aktive / U 18</a></li>
+					  <li><a target="_blank" href="http://slvu23.hlv-region-nord.de/day1.htm">SDM U23 / U16</a></li>
+					  <li><a target="_blank" href="http://lalive.de/150207DESHT">SDHM Aktive / U 18</a></li>
+					  <li><a target="_blank" href="http://www.lg-telis-finanz.de/2015_ergebnisse/2015-09-19-Nieder-Olm.pdf">SDM Senioren MK</a></li>
+				      <li><a target="_blank" href="http://www.blv-sport.de/fileadmin/wettkampfsport/2014/erg/0712_blv-strassengehen.pdf">SDM Gehen</a></li>
+					<li><a href="ergebnisse.php">Ergebnisse der letzten Jahre</a>
+				  </ul></li>
+				   <li<? if($m == 'verband') echo $mA; ?>><a href="verband.php">Verband</a>
+				    <ul>
+				      <li><a href="../praesidium.php">Präsidium</a></li>
+				      <li><a href="../verbaende.php">Mitgliedsverbände  »</a>
+						<ul id="navImpressionen">
+						  <li><a href="http://www.blv-online.de/">Baden</a></li>
+						  <li><a href="http://www.blv-sport.de">Bayern</a></li>
+						  <li><a href="http://www.wlv-sport.de">Württemberg</a></li>
+						  <li><a href="http://www.lv-pfalz.de">Pfalz</a></li>
+						  <li><a href="http://www.hlv.de">Hessen</a></li>
+						  <li><a href="http://www.lvrheinhessen.de">Rheinhessen</a></li>
+						  <li><a href="http://www.slb-saarland.com">Saarland</a></li>
+						</ul></li>
+				      <li><a target="_blank" href="../download/Allgemeine_Ausschreibungsbestimmungen_Aenderung.pdf">Ausschreibungs-<br/>bestimmungen</a></li>
+				      <li><a target="_blank" href="../download/Normen_SDM.pdf">Normen</a></li>
+				      <li><a href="../download/Rotationsplan_2015_2022.pdf">Rotationsplan</a></li>
 				    </ul>
 				  </li>
-				  <li<? if($m == 'ausschreibung') echo $mA; ?>><a href="../ausschreibung.php">Ausschreibung</a>
-				  <ul>
-				  <li<? if($m == 'ausschreibung') echo $mA; ?>><a target="_blank" href="http://slv-leichtathletik.de/Ausschreibungen/Allgemeine_Ausschreibungsbestimmungen_Neufassung_vom_20.10.2012.pdf">Ausschreibungsbe-<br/>stimmungen 2013</a></li>
-				  </ul></li>
-				  <li<? if($m == 'zeitplan') echo $mA; ?>><a href="../zeitplan.php">Zeitplan</a></li>
-				  <li<? if($m == 'teilnehmer') echo $mA; ?>><a href="../teilnehmer.php">Teilnehmer</a></li>
-				  <li<? if($m == 'ergebnisse') echo $mA; ?>><a href="../ergebnisse.php">Ergebnisse</a></li>
-				  <li<? if($m == 'rueckblick') echo $mA; ?>><a target="_blank" href="../download/sueddeutsche_2012-ergebnisse.pdf">Rückblick 2012</a></li>
-				  <li<? if($m == 'sponsoren') echo $mA; ?>><a href="../sponsoren/">Sponsoren</a></li>
 				</ul>
 			</div>
 		</div>
@@ -92,29 +98,26 @@ $mA = $menu['active'];
 			<div id="col1">
 				<div id="col1_content" class="clearfix">
 
-<br />
-<h4>Sponsoren</h4>
-<br />
-<br />
 
-<iframe name="sponsoren" width="190" height="400" frameborder="0" src="../sponsoren/iframe.html" scrolling="no"></iframe>
+          <!--
+          <iframe name="sponsoren" width="190" height="400" frameborder="0" src="sponsoren/iframe.html" scrolling="no"></iframe>
+          -->
 
 
-				</div>
-			</div>
-			<!-- end: #col1 -->
-			<!-- begin: #col2 second float column -->
-			<div id="col2">
-				<div id="col2_content" class="clearfix"> </div>
-			</div>
-			<!-- end: #col2 -->
+          			  </div>
+          			</div>
+          			<!-- end: #col1 -->
+          			<!-- begin: #col2 second float column -->
+          			<div id="col2">
+          				<div id="col2_content" class="clearfix"> </div>
+          			</div>
+          			<!-- end: #col2 -->
 
-			<!-- begin: #col3 Inhaltsspalte (static column) -->
-			<div id="col3">
-				<div id="col3_content" class="clearfix">
-					<!-- skip anchor: content -->
-					<a id="content" name="content"></a><br />
-
-<!-- Besonderes
-<h4 class="hinweis" style="text-align:center;"><a href="http://www.sparkassen-gala.de/2012/">» Seite der Sparkassen Gala 2012</a></h4>
-<br />	-->
+          			<!-- begin: #col3 Inhaltsspalte (static column) -->
+          			<div id="col3">
+          				<div id="col3_content" class="clearfix">
+          					<!-- skip anchor: content -->
+          					<a id="content" name="content"></a><br />
+          <!-- Besonderes
+          <h4 class="hinweis" style="text-align:center;"><a href="http://www.sparkassen-gala.de/2012/">» Seite der Sparkassen Gala 2012</a></h4>
+          <br />	-->
--- articles/SDHM2016.php
+++ articles/SDHM2016.php
@@ -56,45 +56,4 @@
   <p>
     Der direkte Link zum Live-Ergebnisdienst: <a href="http://www.wlv-sport.de/live/2016SLVHalle/">http://www.wlv-sport.de/live/2016SLVHalle/</a>
   </p>
-
-
-    <h2>Süddeutsche Hallenmeisterschaften 2016</h2>
-    <br>
-    <p>
-      Die <a href="/download/2016_01_30-Teilnehmerliste-SDHM-Sindelfingen.htm" target="_blank">Teilnehmerliste</a> und der <a href="/download/SLV-Hallenmeisterschaft-2016-Zeitplan.pdf" target="_blank">Zeitplan</a> der Süddeutschen Hallenmeisterschaften 2016 in Sindelfingen sind online.
-    </p>
-
-
-    <h2>Jochen Schweitzer zum neuen Präsidenten des Süddeutschen Leichtathletik-Verbandes gewählt</h2>
-
-    <p><img class="float_left" src="http://www.blv-sport.de/typo3temp/pics/ec21e4a5b2.jpg" alt="Das neue Präsidium des Süddeutschen Leichtathletik-Verbandes (von links): Paul Blaschke, Hans Krieg, Jochen Schweitzer und Paul Paszyna. (Foto: blv-sport.de)" height="150px"/>
-      Der Süddeutsche Leichtathletik-Verband (SLV) wird ab sofort von einem Präsidenten aus Bayern "regiert". Jochen Schweitzer (33) wurde am Samstag beim Verbandsrat in Bietigheim-Bissingen (Baden-Württemberg) zum Nachfolger des nicht mehr kandidierenden Dr. Ulrich Becker (Pfalz) gewählt. Schweitzer amtiert seit 2011 als SLV-Vizepräsident und ist bei der LG Telis Finanz Regensburg für Marketing und Organisation, unter anderem bei der Sparkassen Gala, zuständig. <a href="http://www.blv-sport.de/index.php?id=377&tx_ttnews[tt_news]=2217&cHash=25487bf282057df87d1e5105de0f4657" target="_blank">Hier</a> geht's weiter auf blv-sport.de
-    </p>
-
-    <h2>Termine</h2>
-    <br />
-    <p>16.01.2016 Süddeutsche Hallenmeisterschaften im Bahngehen in Karlsruhe</p>
-    <p>30.-31.01.2016 SLV-Hallenmeistersch. Aktive/U18 in Sindelfingen</p>
-    <p>12.03.2016 Süddeutsche Meisterschaften Aktive 10.000m (Bahn) in Regensburg</p>
-    <p>25.-26.06.2016 SLV-Meisterschaften Aktive/ U18 in Heilbronn</p>
-    <p>16.-17.07.2016 SLV-Meisterschaften U23/U16 in St. Wendel</p>
-    <p>24.09.2016 SLV-Meisterschaften Seniorenmehrkampf in Nieder-Olm</p>
-    <p>03.09.2016 SLV-Meisterschaften im Bahngehenin Groß Gerau</p>
-    <p>07.05.2016 SLV-Meisterschaften im Straßengehen in Niederaichbach</p>
-
-    <p>
-      <strong>Liebe Sportfreunde,</strong>
-    </p>
-
-    <p>herzlich willkommen auf der Internetpräsenz des Süddeutschen Leichtathletik - Verbandes.</p>
-
-    <p>Auf den Seiten haben wir alle Infos zusammengestellt, die für die Mitgliedsverbände sowie auch deren Mitglieder von Interesse und Wichtigkeit sind.</p>
-
-    <p>Mit sportlichen Grüßen</p>
-
-    <p>Das Präsidium des SLV</p>
-
-    <p></p>
-    <p></p>
-    <br />
     <?php require '../_footer.dir1.inc.php'; ?>
