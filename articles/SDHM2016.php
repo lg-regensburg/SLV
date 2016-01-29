81370b7d7ccc401a0f968288138d85c290147144
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2016-01-29 21:25:10 +0100

    article:weiterlesen

---
+++ articles/SDHM2016.php
@@ -0,0 +1,100 @@
+<?php $title = 'Home';
+ $description = 'Willkommen auf der offiziellen Webseite des Süddeutschen Leichtathletikverbandes';
+ $menu['current'] = 'home'; ?>
+  <?php require '../_header.inc.php'; ?>
+
+  <h2>Süddeutsche Hallenmeisterschaften in Sindelfingen</h2>
+  <br>
+  <p>
+    Die Süddeutschen Leichtathletik-Hallenmeisterschaften am 30. und 31. Januar 2016 bilden den Höhepunkt der diesjährigen Leichtathletik-Saison unter dem Hallendach im Bereich des Württembergischen Leichtathletik-Verbandes. Insgesamt 955 Athletinnen und Athleten von 220 Vereinen aus dem Gebiet der Süddeutschen Leichtathletik-Verbände haben für diese Veranstaltung ihre Meldung abgegeben. Dies verdeutlicht, welch hohe Attraktivität die Süddeutschen Hallenmeisterschaften nach wie vor genießen. 48 Einzeltitel und 7 Staffeltitel in den Klassen Frauen, Männer sowie weibliche und männliche Jugend U18 werden an den beiden Veranstaltungstagen im frisch sanierten Sindelfinger Glaspalast - dem Wohnzimmer der Leichtathletik in Württemberg - vergeben. Für das Team des Ausrichters VfL Sindelfingen also viel Arbeit, die aber mit der Erfahrung vieler Top-Events im Glaspalast engagiert und routiniert geleistet wird; alle Teilnehmer können sich auf eine bestens präparierte Halle und optimale Bedingungen freuen.
+  </p>
+
+  <p>
+    Dabei kommt es zum einen zu einem ersten Aufeinandertreffen eines Teils der deutschen Top-Athleten unter Meisterschaftsbedingungen. Zum anderen stellen die Süddeutschen Meisterschaften für viele Athleten eine willkommene Möglichkeit dar, die Qualifikationsleistung für die Deutschen Hallenmeisterschaften in Leipzig (27./28. Februar) zu erbringen. Spannende Wettkämpfe und hochklassige Leistungen sind also an diesem Wochenende im Glaspalast garantiert.
+  </p>
+
+  <p>
+    WLV-Präsident Jürgen Scholz zeigt sich zufrieden: "Wir sind stolz, dass so viele Spitzenathleten die Süddeutschen Meisterschaften als Startgelegenheit auf dem Weg in die olympische Saison nutzen wollen. Das ist nicht selbstverständlich und untermauert die Bemühungen des Verbandes, des Gastgebers VfL Sindelfingen und der Stadt Sindelfingen Spitzen-Leichtathletik auch weiterhin im Glaspalast zu präsentieren.“
+  </p>
+
+  <p>
+    Heiß umkämpft werden wie immer in der Halle die Entscheidungen im 60 Meter-Sprint sein; allein bei den Männern bewerben sich 81 Sprinter um diesen Titel. Nach den im bisherigen Verlauf der Hallensaison gezeigten Leistungen sind Martin Hahn (ABC Ludwigshafen) und der frisch gekürte Baden-Württembergische Hallenmeister Florian Hochdörffer von der MTG Mannheim favorisiert. Bei den Frauen können unter den 62 gemeldeten Sprinterinnen Ricarda Lobe von der MTG Mannheim und Laura Müller vom LC Rehlingen bislang die schnellsten Zeiten vorweisen. Im Langsprint über die 200 Meter-Hallenrunde darf man gespannt sein, wie sich die beiden jungen Stuttgarter Raphael Müller (VfB) und Emanuel Stubican (TB Bad Cannstatt) gegen die favorisierten Sprinter Rouven Christ und Robert Hind vom LAZ Saar 05 behaupten können. Im 60 Meter Hürdensprint der Männer ist der Mannheimer Neuzugang und Baden-Württembergische Hallenmeister Florian Lickteig in der Favoritenrolle. Er liegt mit seinen am vergangenen Wochenende in Karlsruhe erzielten 7,98 Sekunden bislang auf Platz 2 der DLV-Rangliste. Bei den Frauen darf man auf den ersten Auftritt von Nadine Hildebrand nach langer Verletzungspause sehr gespannt sein. Die Sindelfingerin muss sich dabei mit Landesmeisterin Ricarda Lobe (MTG Mannheim) und ihrer Vereinskameradin Sabrina Lindenmayer auseinandersetzen.
+  </p>
+
+  <p>
+    Schwieriger einzuschätzen sind die Kräfteverhältnisse in den Laufwettbewerben. Über 800 Meter ist Benedikt Huber von der LG Telisfinanz Regensburg stark einzuschätzen aber auch Patrick Oehler vom Gastgeber VfL Sindelfingen will auf heimischer Bahn natürlich beweisen, was er kann. Über 1500 Meter sind Christoph Kessler von der LG Region Karlsruhe, der Frankfurter Nico Sonnenberg aber auch Randy Bögelspacher von der TSG Balingen zu nennen während Martin Sperlich vom VfB LC Friedrichshafen das kleine Feld der 3000 Meter-Läufer dominieren dürfte.
+  </p>
+
+  <p>
+    Über 400 Meter der Frauen könnte es zu einer Neuauflage des Duells der Landesmeisterschaften zwischen Daniela Ferenz (LG Neckar-Enz) und Jackie Baumann (LAV Stadtwerke Tübingen) kommen, in das in Sindelfingen noch die Mannheimerin Hannah Mergenthaler eingreifen könnte. Einen weiteren spannenden Zweikampf verspricht der 1500 Meter-Wettbewerb. Hier haben die U20-Europameisterin Alina Reh (SSV Ulm 1846) und Hanna Klein (SG Schorndorf), die Dritte der Deutschen Meisterschaften des Vorjahres ihre Meldung abgegeben.
+  </p>
+
+  <p>
+    In den Sprungwettbewerben der Männer wird mit Spannung der erste Auftritt von WM-Teilnehmer Fabian Heinle vom VfB Stuttgart erwartet. Er muss sich im Glaspalast mit Landesmeister Julian Howard (LG Region Karlsruhe) auseinandersetzen, der mit 7,83 Meter bislang die deutsche Rangliste anführt. Im Hochsprung will der Deutsche Meister David Nopper vom LAZ Salamander Kornwestheim-Ludwigsburg versuchen, seine 2,22 Meter von den Baden-Württembergischen Hallenmeisterschaften am vergangenen Wochenende im Glaspalast zu toppen und auch im Stabhochsprung ist Landesmeister und Lokalmatador Florian Gaul (VfL Sindelfingen) favorisiert.
+  </p>
+
+  <p>
+    Ein weiterer Höhepunkt verspricht der Hochsprungwettbewerb der Frauen zu werden. Mit Marie-Laurence Jungfleisch vom VfB Stuttgart wird die Sechste der Weltmeisterschaften am Start sein und versuchen, nach ihren 1,94 Meter von vergangenen Wochenende der 2 Meter-Marke in Sindelfingen noch etwas näher zu kommen.
+  </p>
+
+  <p>
+    Im Kugelstoßen der Männer geht es für Tobias Dahm vom VfL Sindelfingen vor heimischem Publikum darum, einen weiteren Schritt in Richtung der 20 Meter-Marke zu machen. Bei den Landesmeisterschaften in Karlsruhe hat er die Saison vielversprechend mit 19,77 Meter begonnen; dass in diesem Jahr für ihn endlich die 20-Meter-Marke fällt, ist also durchaus wahrscheinlich. Zumal der Sindelfinger in Abwesenheit des in diesem Winter pausierenden Olympia-Zweiten David Storl die einmalige Chance hat, Deutscher Hallenmeister zu werden.
+  </p>
+
+  <p>
+    In derselben Situation ist Lena Urbaniak (LG Filstal) bei den Frauen. Auch sie hat die Chance, sich den DM-Titel in der Halle zu holen, da Weltmeisterin Christina Schwanitz die Hallensaison auslässt. Mit ihren 17,12 Metern vom vergangenen Wochenende ist sie auf einem guten Weg dahin. Man darf gespannt sein, ob Lena Urbaniak im Glaspalast noch den einen oder anderen Zentimeter draufpacken kann.
+  </p>
+
+  <p>
+    Große Teilnehmerfelder werden in den Wettbewerben der Jugend U18 am Start sein. Titel-aspiranten aus baden-württembergischer Sicht sind u.a. die beiden Offenburger Sprinter Milo Skupin-Alfa über 60 Meter und Raphael Thoma über 200 Meter, 2-Meter-Hochspringer Devon Bender von der MTG Mannheim, Weitspringer Marcel Cymcyk (SSV Ulm 1846), 800 Meter-Läuferin Leonie Horn (DJK Ellwangen-SG Virngrund) und Kugelstoßerin Lea Riedel (LG Filder).
+  </p>
+
+  <p>
+    Wettkampfbeginn an beiden Veranstaltungstagen ist 11:00 Uhr; der Eintritt ist frei. Weitere Informationen finden Sie auf <a href="www.wlv-sport.de">www.wlv-sport.de</a>
+  </p>
+
+  <p>
+    Der direkte Link zum Live-Ergebnisdienst: <a href="http://www.wlv-sport.de/live/2016SLVHalle/">http://www.wlv-sport.de/live/2016SLVHalle/</a>
+  </p>
+
+
+    <h2>Süddeutsche Hallenmeisterschaften 2016</h2>
+    <br>
+    <p>
+      Die <a href="/download/2016_01_30-Teilnehmerliste-SDHM-Sindelfingen.htm" target="_blank">Teilnehmerliste</a> und der <a href="/download/SLV-Hallenmeisterschaft-2016-Zeitplan.pdf" target="_blank">Zeitplan</a> der Süddeutschen Hallenmeisterschaften 2016 in Sindelfingen sind online.
+    </p>
+
+
+    <h2>Jochen Schweitzer zum neuen Präsidenten des Süddeutschen Leichtathletik-Verbandes gewählt</h2>
+
+    <p><img class="float_left" src="http://www.blv-sport.de/typo3temp/pics/ec21e4a5b2.jpg" alt="Das neue Präsidium des Süddeutschen Leichtathletik-Verbandes (von links): Paul Blaschke, Hans Krieg, Jochen Schweitzer und Paul Paszyna. (Foto: blv-sport.de)" height="150px"/>
+      Der Süddeutsche Leichtathletik-Verband (SLV) wird ab sofort von einem Präsidenten aus Bayern "regiert". Jochen Schweitzer (33) wurde am Samstag beim Verbandsrat in Bietigheim-Bissingen (Baden-Württemberg) zum Nachfolger des nicht mehr kandidierenden Dr. Ulrich Becker (Pfalz) gewählt. Schweitzer amtiert seit 2011 als SLV-Vizepräsident und ist bei der LG Telis Finanz Regensburg für Marketing und Organisation, unter anderem bei der Sparkassen Gala, zuständig. <a href="http://www.blv-sport.de/index.php?id=377&tx_ttnews[tt_news]=2217&cHash=25487bf282057df87d1e5105de0f4657" target="_blank">Hier</a> geht's weiter auf blv-sport.de
+    </p>
+
+    <h2>Termine</h2>
+    <br />
+    <p>16.01.2016 Süddeutsche Hallenmeisterschaften im Bahngehen in Karlsruhe</p>
+    <p>30.-31.01.2016 SLV-Hallenmeistersch. Aktive/U18 in Sindelfingen</p>
+    <p>12.03.2016 Süddeutsche Meisterschaften Aktive 10.000m (Bahn) in Regensburg</p>
+    <p>25.-26.06.2016 SLV-Meisterschaften Aktive/ U18 in Heilbronn</p>
+    <p>16.-17.07.2016 SLV-Meisterschaften U23/U16 in St. Wendel</p>
+    <p>24.09.2016 SLV-Meisterschaften Seniorenmehrkampf in Nieder-Olm</p>
+    <p>03.09.2016 SLV-Meisterschaften im Bahngehenin Groß Gerau</p>
+    <p>07.05.2016 SLV-Meisterschaften im Straßengehen in Niederaichbach</p>
+
+    <p>
+      <strong>Liebe Sportfreunde,</strong>
+    </p>
+
+    <p>herzlich willkommen auf der Internetpräsenz des Süddeutschen Leichtathletik - Verbandes.</p>
+
+    <p>Auf den Seiten haben wir alle Infos zusammengestellt, die für die Mitgliedsverbände sowie auch deren Mitglieder von Interesse und Wichtigkeit sind.</p>
+
+    <p>Mit sportlichen Grüßen</p>
+
+    <p>Das Präsidium des SLV</p>
+
+    <p></p>
+    <p></p>
+    <br />
+    <?php require '../_footer.inc.php'; ?>
--- index.php
+++ index.php
@@ -6,57 +6,8 @@
   <h2>Süddeutsche Hallenmeisterschaften in Sindelfingen</h2>
   <br>
   <p>
-    Die Süddeutschen Leichtathletik-Hallenmeisterschaften am 30. und 31. Januar 2016 bilden den Höhepunkt der diesjährigen Leichtathletik-Saison unter dem Hallendach im Bereich des Württembergischen Leichtathletik-Verbandes. Insgesamt 955 Athletinnen und Athleten von 220 Vereinen aus dem Gebiet der Süddeutschen Leichtathletik-Verbände haben für diese Veranstaltung ihre Meldung abgegeben. Dies verdeutlicht, welch hohe Attraktivität die Süddeutschen Hallenmeisterschaften nach wie vor genießen. 48 Einzeltitel und 7 Staffeltitel in den Klassen Frauen, Männer sowie weibliche und männliche Jugend U18 werden an den beiden Veranstaltungstagen im frisch sanierten Sindelfinger Glaspalast - dem Wohnzimmer der Leichtathletik in Württemberg - vergeben. Für das Team des Ausrichters VfL Sindelfingen also viel Arbeit, die aber mit der Erfahrung vieler Top-Events im Glaspalast engagiert und routiniert geleistet wird; alle Teilnehmer können sich auf eine bestens präparierte Halle und optimale Bedingungen freuen.
+    Die Süddeutschen Leichtathletik-Hallenmeisterschaften am 30. und 31. Januar 2016 bilden den Höhepunkt der diesjährigen Leichtathletik-Saison unter dem Hallendach im Bereich des Württembergischen Leichtathletik-Verbandes. Insgesamt 955 Athletinnen und Athleten von 220 Vereinen aus dem Gebiet der Süddeutschen Leichtathletik-Verbände haben für diese Veranstaltung ihre Meldung abgegeben. Dies verdeutlicht, welch hohe Attraktivität die Süddeutschen Hallenmeisterschaften nach wie vor genießen. 48 Einzeltitel und 7 Staffeltitel in den Klassen Frauen, Männer sowie weibliche und männliche Jugend U18 werden an den beiden Veranstaltungstagen im frisch sanierten Sindelfinger Glaspalast - dem Wohnzimmer der Leichtathletik in Württemberg - vergeben. Für das Team des Ausrichters VfL Sindelfingen also viel Arbeit, die aber mit der Erfahrung vieler Top-Events im Glaspalast engagiert und routiniert geleistet wird; alle Teilnehmer können sich auf eine bestens präparierte Halle und optimale Bedingungen freuen. <a href="articles/SDHM2016.php">Weiterlesen...</a>
   </p>
-
-  <p>
-    Dabei kommt es zum einen zu einem ersten Aufeinandertreffen eines Teils der deutschen Top-Athleten unter Meisterschaftsbedingungen. Zum anderen stellen die Süddeutschen Meisterschaften für viele Athleten eine willkommene Möglichkeit dar, die Qualifikationsleistung für die Deutschen Hallenmeisterschaften in Leipzig (27./28. Februar) zu erbringen. Spannende Wettkämpfe und hochklassige Leistungen sind also an diesem Wochenende im Glaspalast garantiert.
-  </p>
-
-  <p>
-    WLV-Präsident Jürgen Scholz zeigt sich zufrieden: "Wir sind stolz, dass so viele Spitzenathleten die Süddeutschen Meisterschaften als Startgelegenheit auf dem Weg in die olympische Saison nutzen wollen. Das ist nicht selbstverständlich und untermauert die Bemühungen des Verbandes, des Gastgebers VfL Sindelfingen und der Stadt Sindelfingen Spitzen-Leichtathletik auch weiterhin im Glaspalast zu präsentieren.“
-  </p>
-
-  <p>
-    Heiß umkämpft werden wie immer in der Halle die Entscheidungen im 60 Meter-Sprint sein; allein bei den Männern bewerben sich 81 Sprinter um diesen Titel. Nach den im bisherigen Verlauf der Hallensaison gezeigten Leistungen sind Martin Hahn (ABC Ludwigshafen) und der frisch gekürte Baden-Württembergische Hallenmeister Florian Hochdörffer von der MTG Mannheim favorisiert. Bei den Frauen können unter den 62 gemeldeten Sprinterinnen Ricarda Lobe von der MTG Mannheim und Laura Müller vom LC Rehlingen bislang die schnellsten Zeiten vorweisen. Im Langsprint über die 200 Meter-Hallenrunde darf man gespannt sein, wie sich die beiden jungen Stuttgarter Raphael Müller (VfB) und Emanuel Stubican (TB Bad Cannstatt) gegen die favorisierten Sprinter Rouven Christ und Robert Hind vom LAZ Saar 05 behaupten können. Im 60 Meter Hürdensprint der Männer ist der Mannheimer Neuzugang und Baden-Württembergische Hallenmeister Florian Lickteig in der Favoritenrolle. Er liegt mit seinen am vergangenen Wochenende in Karlsruhe erzielten 7,98 Sekunden bislang auf Platz 2 der DLV-Rangliste. Bei den Frauen darf man auf den ersten Auftritt von Nadine Hildebrand nach langer Verletzungspause sehr gespannt sein. Die Sindelfingerin muss sich dabei mit Landesmeisterin Ricarda Lobe (MTG Mannheim) und ihrer Vereinskameradin Sabrina Lindenmayer auseinandersetzen.
-  </p>
-
-  <p>
-    Schwieriger einzuschätzen sind die Kräfteverhältnisse in den Laufwettbewerben. Über 800 Meter ist Benedikt Huber von der LG Telisfinanz Regensburg stark einzuschätzen aber auch Patrick Oehler vom Gastgeber VfL Sindelfingen will auf heimischer Bahn natürlich beweisen, was er kann. Über 1500 Meter sind Christoph Kessler von der LG Region Karlsruhe, der Frankfurter Nico Sonnenberg aber auch Randy Bögelspacher von der TSG Balingen zu nennen während Martin Sperlich vom VfB LC Friedrichshafen das kleine Feld der 3000 Meter-Läufer dominieren dürfte.
-  </p>
-
-  <p>
-    Über 400 Meter der Frauen könnte es zu einer Neuauflage des Duells der Landesmeisterschaften zwischen Daniela Ferenz (LG Neckar-Enz) und Jackie Baumann (LAV Stadtwerke Tübingen) kommen, in das in Sindelfingen noch die Mannheimerin Hannah Mergenthaler eingreifen könnte. Einen weiteren spannenden Zweikampf verspricht der 1500 Meter-Wettbewerb. Hier haben die U20-Europameisterin Alina Reh (SSV Ulm 1846) und Hanna Klein (SG Schorndorf), die Dritte der Deutschen Meisterschaften des Vorjahres ihre Meldung abgegeben.
-  </p>
-
-  <p>
-    In den Sprungwettbewerben der Männer wird mit Spannung der erste Auftritt von WM-Teilnehmer Fabian Heinle vom VfB Stuttgart erwartet. Er muss sich im Glaspalast mit Landesmeister Julian Howard (LG Region Karlsruhe) auseinandersetzen, der mit 7,83 Meter bislang die deutsche Rangliste anführt. Im Hochsprung will der Deutsche Meister David Nopper vom LAZ Salamander Kornwestheim-Ludwigsburg versuchen, seine 2,22 Meter von den Baden-Württembergischen Hallenmeisterschaften am vergangenen Wochenende im Glaspalast zu toppen und auch im Stabhochsprung ist Landesmeister und Lokalmatador Florian Gaul (VfL Sindelfingen) favorisiert.
-  </p>
-
-  <p>
-    Ein weiterer Höhepunkt verspricht der Hochsprungwettbewerb der Frauen zu werden. Mit Marie-Laurence Jungfleisch vom VfB Stuttgart wird die Sechste der Weltmeisterschaften am Start sein und versuchen, nach ihren 1,94 Meter von vergangenen Wochenende der 2 Meter-Marke in Sindelfingen noch etwas näher zu kommen.
-  </p>
-
-  <p>
-    Im Kugelstoßen der Männer geht es für Tobias Dahm vom VfL Sindelfingen vor heimischem Publikum darum, einen weiteren Schritt in Richtung der 20 Meter-Marke zu machen. Bei den Landesmeisterschaften in Karlsruhe hat er die Saison vielversprechend mit 19,77 Meter begonnen; dass in diesem Jahr für ihn endlich die 20-Meter-Marke fällt, ist also durchaus wahrscheinlich. Zumal der Sindelfinger in Abwesenheit des in diesem Winter pausierenden Olympia-Zweiten David Storl die einmalige Chance hat, Deutscher Hallenmeister zu werden.
-  </p>
-
-  <p>
-    In derselben Situation ist Lena Urbaniak (LG Filstal) bei den Frauen. Auch sie hat die Chance, sich den DM-Titel in der Halle zu holen, da Weltmeisterin Christina Schwanitz die Hallensaison auslässt. Mit ihren 17,12 Metern vom vergangenen Wochenende ist sie auf einem guten Weg dahin. Man darf gespannt sein, ob Lena Urbaniak im Glaspalast noch den einen oder anderen Zentimeter draufpacken kann.
-  </p>
-
-  <p>
-    Große Teilnehmerfelder werden in den Wettbewerben der Jugend U18 am Start sein. Titel-aspiranten aus baden-württembergischer Sicht sind u.a. die beiden Offenburger Sprinter Milo Skupin-Alfa über 60 Meter und Raphael Thoma über 200 Meter, 2-Meter-Hochspringer Devon Bender von der MTG Mannheim, Weitspringer Marcel Cymcyk (SSV Ulm 1846), 800 Meter-Läuferin Leonie Horn (DJK Ellwangen-SG Virngrund) und Kugelstoßerin Lea Riedel (LG Filder).
-  </p>
-
-  <p>
-    Wettkampfbeginn an beiden Veranstaltungstagen ist 11:00 Uhr; der Eintritt ist frei. Weitere Informationen finden Sie auf <a href="www.wlv-sport.de">www.wlv-sport.de</a>
-  </p>
-
-  <p>
-    Der direkte Link zum Live-Ergebnisdienst: <a href="http://www.wlv-sport.de/live/2016SLVHalle/">http://www.wlv-sport.de/live/2016SLVHalle/</a>
-  </p>
-
 
     <h2>Süddeutsche Hallenmeisterschaften 2016</h2>
     <br>
