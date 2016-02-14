741466bb3b41ad49b0476f6e3714fa5fc81a9bed
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2016-02-14 10:49:20 +0100

    Ausschreibungen aktualisiert

--- ausschreibung-wird-in-kuerze-eingestellt.php
+++
@@ -1,9 +0,0 @@
-<?php $title = "Ausschreibungen";
- $description = "Ausschreibungen";
- $menu['current'] = "infos"; ?>
-  <?php require("_header.inc.php"); ?>
-
-    <h2>Die Ausschreibung wird in Kürze eingestellt</h2>
-    <br />
-
-<?php require("_footer.inc.php"); ?>
