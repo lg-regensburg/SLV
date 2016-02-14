89c4d4fc0dfb7a4f653a0df263f7e23a4543543b
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2016-02-14 10:44:28 +0100

    Veranstaltungen aktualisiert

--- veranstaltungen.php
+++ veranstaltungen.php
@@ -9,12 +9,14 @@
     <p>16.01.2016 Süddeutsche Hallenmeisterschaften im Bahngehen in Karlsruhe</p>
     <p>30.-31.01.2016 SLV-Hallenmeistersch. Aktive/U18 in Sindelfingen (<a target="_blank" href="/download/Ausschreibung_SDHM_2016.pdf">Ausschreibung</a>)</p>
     <p>12.03.2016 Süddeutsche Meisterschaften Aktive 10.000m (Bahn) in Regensburg</p>
-    <p>07.05.2016 SLV-Meisterschaften im Straßengehen in Niederaichbach (<a target="_blank" href="/ausschreibung-wird-in-kuerze-eingestellt.php">Ausschreibung</a>)</p>
+    <p>07.05.2016 SLV-Meisterschaften im Straßengehen in Niederaichbach (<a target="_blank" href="/ausschreibung-wird-in-kuerze-eingestellt.php">Ausschreibung</a>, <a href="http://www.blv-sport.de/index.php?id=242&vid=20166557" target="_blank">Veranstaltungsinformationen</a>)</p>
     <p>25.-26.06.2016 SLV-Meisterschaften Aktive/ U18 in Heilbronn (<a target="_blank" href="/ausschreibung-wird-in-kuerze-eingestellt.php ">Ausschreibung</a>)</p>
     <p>16.-17.07.2016 SLV-Meisterschaften U23/U16 in St. Wendel (<a target="_blank" href="/ausschreibung-wird-in-kuerze-eingestellt.php">Ausschreibung</a>)</p>
     <p>24.09.2016 SLV-Meisterschaften Seniorenmehrkampf in Nieder-Olm (<a target="_blank" href="/ausschreibung-wird-in-kuerze-eingestellt.php">Ausschreibung</a>)</p>
-    <p>03.09.2016 SLV-Meisterschaften im Bahngehen in Groß Gerau (<a target="_blank" href="/ausschreibung-wird-in-kuerze-eingestellt.php">Ausschreibung</a>)</p>
+    <p>03.09.2016 SLV-Meisterschaften im Bahngehen in Groß Gerau (<a target="_blank" href="/ausschreibung-wird-in-kuerze-eingestellt.php">Ausschreibung</a>, <a href="http://www.hlv.de/WETTKAMPFSPORT/view_termin.asp?id=975" target="_blank">Veranstaltungsinformationen</a>)</p>
 
+    <p></p>
+    <p></p>
 
     <p><a target="_blank" href="/download/Allgemeine%20Ausschreibungsbestimmungen%20Aenderung.pdf">Ausschreibungsbestimmungen</a></p>
     <p><a target="_blank" href="/download/Normen_SDM.pdf">Normen</a></p>
