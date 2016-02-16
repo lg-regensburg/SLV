878c81a72b86606c4865e00a74e90021c23d5737
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2016-02-16 07:48:21 +0100

    Ausschreibung 10000m aktualisiert

--- veranstaltungen.php
+++ veranstaltungen.php
@@ -8,7 +8,7 @@
     <br />
     <p>16.01.2016 Süddeutsche Hallenmeisterschaften im Bahngehen in Karlsruhe</p>
     <p>30.-31.01.2016 SLV-Hallenmeistersch. Aktive/U18 in Sindelfingen</p>
-    <p>12.03.2016 Süddeutsche Meisterschaften Aktive 10.000m (Bahn) in Regensburg (<a href="http://www.lg-telis-finanz.de/2016_ausschreibungen/03_12_blv-10000m.pdf">Ausschreibung</a>)</p>
+    <p>12.03.2016 Süddeutsche Meisterschaften Aktive 10.000m (Bahn) in Regensburg (<a href="http://ladv.de/ausschreibung/datei/24471">Ausschreibung</a>)</p>
     <p>07.05.2016 SLV-Meisterschaften im Straßengehen in Niederaichbach (<a href="http://www.blv-sport.de/index.php?id=242&vid=20166557" target="_blank">Ausschreibung</a>)</p>
     <p>25.-26.06.2016 SLV-Meisterschaften Aktive/ U18 in Heilbronn (die Ausschreibung wird in Kürze zur Verfügung gestellt, <a href="http://blv-sport.de/index.php?id=242&vid=20166622" target="_blank">Veranstaltungsinformationen</a>)</p>
     <p>16.-17.07.2016 SLV-Meisterschaften U23/U16 in St. Wendel (die Ausschreibung wird in Kürze zur Verfügung gestellt, <a href="http://blv-sport.de/index.php?id=242&vid=20166624" target="_blank">Veranstaltungsinformationen</a>)</p>
