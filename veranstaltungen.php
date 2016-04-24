dcca4204d7172612d2ac2e2af06a1cc3a5f8af18
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2016-04-24 22:50:36 +0200

    add ausschreibung

--- veranstaltungen.php
+++ veranstaltungen.php
@@ -11,7 +11,7 @@
     <p>12.03.2016 Süddeutsche Meisterschaften Aktive 10.000m (Bahn) in Regensburg (<a href="http://ladv.de/ausschreibung/datei/24471">Ausschreibung</a>)</p>
     <p>07.05.2016 SLV-Meisterschaften im Straßengehen in Niederaichbach (<a href="http://www.blv-sport.de/index.php?id=242&vid=20166557" target="_blank">Ausschreibung</a>)</p>
     <p>25.-26.06.2016 SLV-Meisterschaften Aktive/ U18 in Heilbronn (<a href="/download/SLV Aktive-U18_Heilbronn 2016.pdf" target="_blank">Ausschreibung</a>)</p>
-    <p>16.-17.07.2016 SLV-Meisterschaften U23/U16 in St. Wendel (die Ausschreibung wird in Kürze zur Verfügung gestellt, <a href="http://blv-sport.de/index.php?id=242&vid=20166624" target="_blank">Veranstaltungsinformationen</a>)</p>
+    <p>16.-17.07.2016 SLV-Meisterschaften U23/U16 in St. Wendel  (<a href="/download/Ausschreibung_SDM_U23_U16.pdf" target="_blank">Ausschreibung</a>)</p>
     <p>24.09.2016 SLV-Meisterschaften Seniorenmehrkampf in Nieder-Olm (die Ausschreibung wird in Kürze zur Verfügung gestellt, <a href="http://blv-sport.de/index.php?id=242&vid=20166625" target="_blank">Veranstaltungsinformationen</a>)</p>
     <p>03.09.2016 SLV-Meisterschaften im Bahngehen in Groß Gerau (die Ausschreibung wird in Kürze zur Verfügung gestellt, <a href="http://www.hlv.de/WETTKAMPFSPORT/view_termin.asp?id=975" target="_blank">Veranstaltungsinformationen</a>)</p>
 
