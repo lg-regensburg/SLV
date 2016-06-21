36cd91dd4872cfb2af1b1513a9d7a20674d559dc
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2016-06-21 22:34:53 +0200

    update Zeitplan

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
  $menu['current'] = 'home'; ?>
   <?php require '_header.inc.php'; ?>
 
+    <h2>Aktualisierter Zeitplan SDM Männer, Frauen, U18</h2>
+    <br>
+    <p>
+      Der Zeitplan der Süddeutschen Meisterschaften in Heilbronn wurde aktualisiert und steht <a href="download/Zeitplan%20SLV%20Aktive-U18_Heilbronn%202016_Final.pdf">hier</a> zur Verfügung.
+    </p>
+
     <h2>Termine</h2>
     <br />
     <p>25.-26.06.2016 SLV-Meisterschaften Aktive/ U18 in Heilbronn</p>
