f635ec24f3fabc701cdceb1fbd9936c79e3f263e
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2016-07-11 16:54:58 +0200

    meldeliste sdm

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
  $menu['current'] = 'home'; ?>
   <?php require '_header.inc.php'; ?>
 
+    <p>
+      <strong>Meldeliste Süddeutsche Meisterschaften U23 / U16 online</strong>
+    </p>
+    <br>
+    <p>
+      Die Meldelisten der Süddeutschen  Meisterschaften U23 / U16 sind <a href="http://www.slb-saarland.com/fileadmin/user_upload/Termine/2016/07_juli/sdm/20160716stwendel/20160716disziplinen.htm" target="_blank">Hier</a> verfügbar.
+    </p>
     <h2>Termine</h2>
     <br />
     <p>25.-26.06.2016 SLV-Meisterschaften Aktive/ U18 in Heilbronn</p>
